2aa2e018ca897a036c616e50156b1581c5302b24
koorosh ahmadi <81431046+kooroshahmadi@users.noreply.github.com>
2025-12-06 17:03:39 +0330

    Update Proxmox provider configuration and comments

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_endpoint
-  pm_api_token_id     = split("=", var.proxmox_token)[0]
-  pm_api_token_secret = split("=", var.proxmox_token)[1]
-  pm_tls_insecure     = true
+  pm_api_url      = var.proxmox_endpoint
+  pm_user         = var.proxmox_user
+  pm_password     = var.proxmox_password
+  pm_tls_insecure = true
 }
 
-# 1. Trigger: Detects if config changes
+# 1. Trigger: Detects if config changes (RAM, Cores, Packages)
 resource "null_resource" "vm_config_trigger" {
   for_each = var.vms
   triggers = {
@@ -27,9 +27,9 @@ resource "proxmox_vm_qemu" "alpine_vm" {
   for_each = var.vms
 
   name        = each.key
-  target_node = "proxmox1"            # UPDATE THIS if your node isn't named 'pve'
+  target_node = "pve"            # CHANGE THIS if your Proxmox node is not named 'pve'
   vmid        = each.value.vmid
-  clone       = "alpine-template" # We will create this template in Step 2
+  clone       = "alpine-template" 
 
   cores  = each.value.cores
   memory = each.value.memory
@@ -40,14 +40,13 @@ resource "proxmox_vm_qemu" "alpine_vm" {
     bridge = "vmbr0"
   }
 
+  # Cloud-Init Settings
   os_type = "cloud-init"
-  
-  # Standard Alpine Cloud images usually use the 'alpine' user or allow root via keys.
-  # We will configure the SSH connection to use the key generated in Step 3.
   ciuser  = "root"
   sshkeys = file("~/.ssh/id_rsa.pub") 
+  ipconfig0 = "ip=dhcp"
 
-  # SAFETY: Destroy and Recreate if config changes
+  # SAFETY: Destroy and Recreate if config trigger changes
   lifecycle {
     replace_triggered_by = [
       null_resource.vm_config_trigger[each.key]
@@ -58,18 +57,16 @@ resource "proxmox_vm_qemu" "alpine_vm" {
   bootdisk = "scsi0"
   disk {
     storage = "local-lvm"
-    size    = "4G" # Alpine is small
+    size    = "4G"
     type    = "scsi"
   }
-  
-  # Ensure we get an IP before provisioner tries to connect
-  ipconfig0 = "ip=dhcp"
 }
 
-# 3. Provisioning: Install Alpine Packages
+# 3. Provisioning: Install Alpine Packages via SSH
 resource "null_resource" "provisioning" {
   for_each = var.vms
 
+  # Wait for VM to be created first
   depends_on = [proxmox_vm_qemu.alpine_vm]
 
   triggers = {
@@ -85,7 +82,7 @@ resource "null_resource" "provisioning" {
 
   provisioner "remote-exec" {
     inline = [
-      # Wait a moment for network to stabilize
+      # Wait a few seconds for network/DNS to stabilize
       "sleep 5",
       "apk update",
       "apk add ${join(" ", each.value.packages)}"
